afe8415f43a3ad57f5348adb5b1e23996d9cd83c
Najathi <najathi777@gmail.com>
2021-01-30 12:58:54 +0530

    initial

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-"# box-shadow-on-image" 
+# box-shadow-on-image
+
+## What is this?
+
+Get perfect shadows every time for the non-designer
+
+## Installation
+
+`npm i boxshadowonimage --save`
+
+Then...
+
+```
+import {boxshadowonimage} from 'boxshadowonimage';
+
+boxshadowonimage({
+    shadow_type: 'soft',
+    padding: false
+});
+```
+
+### Options
+
+boxshadowonimage supports 2 options, both of which are optional:
+
+* *shadow_type* - _hard | soft_ (Defaults to soft)
+* *padding* - _boolean_ (Defaults to false)
---
+++ index.js
@@ -0,0 +1,24 @@
+function boxShadow(options) {
+
+    let images = document.querySelector('.box-shadow-on-image');
+
+    if (options.shadow_type === 'hard') {
+        options.shadow_type = '0px';
+    }
+    else {
+        options.shadow_type = '15px';
+    }
+
+    images.forEach(image => {
+
+        image.style.boxShadow = `10px 10px ${options.shadow_type} 1px rgba(0,0,0,0.12)`;
+
+        if (options.padding) {
+            image.style.padding = '1rem';
+        }
+
+    });
+
+}
+
+module.exports.boxShadow = boxShadow;
